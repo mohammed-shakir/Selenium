39b501588c44f4f02295bd02bdfafc0283e6ba5b
Mohammed Shakir <mohammedshakir010528@gmail.com>
2023-01-16 00:02:24 +0100

    main

--- test.py
+++ test.py
@@ -2,28 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
+import unittest
 
-def test_edge():
-    driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+        self.driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    
+    def test_example(self):
+        self.driver.implicitly_wait(3)
 
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+        text_box = self.driver.find_element(By.NAME, "my-text")
+        submit_button = self.driver.find_element(By.CSS_SELECTOR, "button")
 
-    driver.implicitly_wait(3)
+        text_box.send_keys("Selenium")
+        submit_button.click()
 
-    text_box = driver.find_element(By.NAME, "my-text")
-    submit_button = driver.find_element(By.CSS_SELECTOR, "button")
+        message = self.driver.find_element(By.ID, "message")
+        value = message.text
 
-    text_box.send_keys("Selenium")
-    submit_button.click()
+        if value == "Received!":
+            print("Test passed")
+        else:
+            print("Test failed")
 
-    message = driver.find_element(By.ID, "message")
-    value = message.text
 
-    if value == "Received!":
-        print("Test passed")
-    else:
-        print("Test failed")
+    def tearDown(self):
+        self.driver.close()
 
-    driver.quit()
-
-test_edge()
+if __name__ == "__main__":
+    unittest.main()
